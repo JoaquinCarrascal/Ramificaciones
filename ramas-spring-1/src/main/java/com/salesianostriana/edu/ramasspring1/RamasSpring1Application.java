5154006a4dc10f4047ee8638a7741266f94c9388
Joaquín Carrascal Franco <carrascal.frjoa24@triana.salesianos.edu>
2024-04-22 13:16:46 +0200

    primer controlador

---
+++ ramas-spring-1/src/main/java/com/salesianostriana/edu/ramasspring1/RamasSpring1Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.edu.ramasspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamasSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamasSpring1Application.class, args);
+	}
+
+}
